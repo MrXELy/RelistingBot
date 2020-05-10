103ffa962248d01925164381bea81a81dcd7f503
MrXELy <misterxely@gmail.com>
2020-05-10 15:33:38 +0200

    Remove headless arg

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ from time import sleep
 from getpass import getpass
 import argparse
 
-# TODO logging https://docs.python.org/3/howto/logging.html
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-u", "--username", help="FUT EA account email")
     parser.add_argument("-p", "--password", help="FUT EA account password")
-    parser.add_argument("--headless", help="Run headless", action="store_true")
 
     args = parser.parse_args()
 
